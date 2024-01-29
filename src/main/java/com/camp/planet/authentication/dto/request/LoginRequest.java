b4024343a03ba87f0b9ccc3dc320a6175ee840c5
yuihmoo <yuihmoo@gmail.com>
2024-01-29 23:13:40 +0900

    first commit

--- src/main/java/com/camp/planet/authentication/dto/request/LoginRequest.java
+++ src/main/java/com/camp/planet/authentication/dto/request/LoginRequest.java
@@ -1,5 +1,7 @@
 package com.camp.planet.authentication.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
+    @NotBlank(message = "로그인 아이디는 필수 입니다.")
+    @Size(min = 6, max = 12, message = "최소 6 ~ 12자 영문, 숫자 조합이여야 합니다.")
     private String loginId;
+
+    //todo: 패턴 검증
+    @NotBlank(message = "비밀번호는 필수 입니다.")
     private String password;
 }
