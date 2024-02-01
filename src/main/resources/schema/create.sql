f3571df211774d1f1f31c1d1c419c4a7d0d783ed
yuihmoo <yuihmoo@gmail.com>
2024-02-01 18:04:47 +0900

    first commit

--- src/main/resources/schema/create.sql
+++ src/main/resources/schema/create.sql
@@ -15,3 +15,39 @@ CREATE TABLE account (
 	created_date TIMESTAMP NOT NULL,
     updated_date TIMESTAMP
 );
+
+CREATE TABLE site (
+	id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+	account_id UUID NOT NULL,
+	name VARCHAR(20) NOT NULL,
+	address VARCHAR(50) NOT NULL,
+	phone VARCHAR(11) NOT NULL,
+	type VARCHAR(10) NOT NULL,
+	environment VARCHAR(128) NOT NULL,
+	introduce VARCHAR(100) NOT NULL,
+	manner_time VARCHAR(100) NOT NULL,
+	profile VARCHAR(255) NOT NULL,
+	site_desc_img VARCHAR(255) NOT NULL,
+	created_date TIMESTAMP NOT NULL,
+    updated_date TIMESTAMP
+);
+
+CREATE TABLE zone (
+	id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+	site_id UUID NOT NULL,
+	name VARCHAR(20) UNIQUE NOT NULL,
+	profile VARCHAR(255) NOT NULL,
+	introduce VARCHAR(100) NOT NULL,
+	price VARCHAR(10) NOT NULL,
+	check_in TIMESTAMP NOT NULL,
+	check_out TIMESTAMP NOT NULL,
+	type VARCHAR(100) NOT NULL,
+	floor_material VARCHAR(255) NOT NULL,
+	recommend_people VARCHAR(255) NOT NULL,
+	is_parking BOOLEAN NOT NULL,
+	min_reservation_day SMALLINT NOT NULL,
+	width NUMERIC NOT NULL,
+	height NUMERIC NOT NULL,
+	created_date TIMESTAMP NOT NULL,
+    updated_date TIMESTAMP
+);
